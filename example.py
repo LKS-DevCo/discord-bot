1c82d6b10544e64540ab1bac70106c353cefcb9d
Stephon Patton <stephonpatton@icloud.com>
2022-07-06 22:21:17 -0500

    DB-14 Task complete - Removed token and stopped tracking of .env locally - Removed unnecessary lines

--- example.py
+++ example.py
@@ -1,10 +1,8 @@
-# This example requires the 'message_content' intent.
 import os
 import discord
 from dotenv import load_dotenv
 
 intents = discord.Intents.default()
-# intents.message_content = True
 
 client = discord.Client(intents=intents)
 
